05951eb487737ffb6705a3a3fab369e29a0bcbe8
itosho <sho.ito@connehito.com>
2020-04-14 04:46:22 +0900

    Update v0.2.3

--- gdp.rb
+++ gdp.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Gdp < Formula
   homepage "https://github.com/Connehito/gdp"
-  url "https://github.com/Connehito/gdp/releases/download/v0.2.2/gdp_v0.2.2_darwin_amd64.tar.gz"
-  sha256 "a42f8dbc03ddc2e12be51cbb07c44d059e0bffa67b67b7fb162d9eef1a2b7fcf"
+  url "https://github.com/Connehito/gdp/releases/download/v0.2.3/gdp_v0.2.3_darwin_amd64.tar.gz"
+  sha256 "6f1a4cb01f0f01f5eb921e4bb64250efd488fd01b9c0b8985e4919de77525f85"
   head "https://github.com/Connehito/gdp.git"
-  version "0.2.2"
+  version "0.2.3"
 
   depends_on "hub"
   depends_on "git"
