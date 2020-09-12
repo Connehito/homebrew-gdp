6116c1d591a89fb8480785ec6fa0b29e59fedaf1
itosho <www.itosho@gmail.com>
2020-09-13 03:27:30 +0900

    Fix hash

--- gdp.rb
+++ gdp.rb
@@ -3,7 +3,7 @@ require "formula"
 class Gdp < Formula
   homepage "https://github.com/Connehito/gdp"
   url "https://github.com/Connehito/gdp/releases/download/v0.2.4/gdp_v0.2.4_darwin_amd64.tar.gz"
-  sha256 "6f513e2cc65c58596c09c5de6ccf03382e92a878"
+  sha256 "4f31c4ef000016056922bd33eab5cf6957c519d10338d80277dc10b07924c613"
   head "https://github.com/Connehito/gdp.git"
   version "0.2.4"
 
