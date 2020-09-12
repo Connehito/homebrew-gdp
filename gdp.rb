b2007308d1207224c0e89b9a208f58881ff4662c
itosho <www.itosho@gmail.com>
2020-09-13 03:34:55 +0900

    Update gdp.rb

--- gdp.rb
+++ gdp.rb
@@ -3,7 +3,7 @@ require "formula"
 class Gdp < Formula
   homepage "https://github.com/Connehito/gdp"
   url "https://github.com/Connehito/gdp/releases/download/v0.2.4/gdp_v0.2.4_darwin_amd64.tar.gz"
-  sha256 "4f31c4ef000016056922bd33eab5cf6957c519d10338d80277dc10b07924c613"
+  sha256 "fd784c157a38667ecf2d8dc5b5a4b49f98e0114555476a4940b5f3bd497540c4"
   head "https://github.com/Connehito/gdp.git"
   version "0.2.4"
 
